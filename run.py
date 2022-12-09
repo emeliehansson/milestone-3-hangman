207efbd164757038bf286a2aadfdab3a0b6cf703
emeliehansson <emeliehansson_@hotmail.com>
2022-12-09 09:43:26 +0000

    Added more content and a while loop to the play_options function. Also added a how_to_play function with instructions for the user

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import random
 from words import word_list
 
 
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+
 def welcome_message():
 
     """
@@ -17,10 +22,7 @@ def welcome_message():
 
     print('Press 1 to start new game')
     print('Press 2 for the rules')
-    print('Press 3 to choose difficulty(easy,medium,hard)')
-
-
-print(welcome_message)
+    print('Press 3 to choose difficulty')
 
 
 def display_hangman(tries):
@@ -136,20 +138,54 @@ def display_hangman(tries):
 
 def play_options():
     """
-    Startup view, shows the "are you ready to play hangman?!" text and 
+    Startup view, shows the "are you ready to play hangman?!" text and
     also gives the player the different options to choose from.
+    User choice is displayed in the welcome message function.
     """
     print(welcome_message())
 
+    start = False
 
-def get_word():
-    word = random.choice(word_list)
-    return word.upper()
+    while not start:
+        choice = input('\n')
+        if choice == '1':
+            start = True
+            difficulty = 'default'
+            return difficulty
+        
+        elif choice == '2':
+            start = True
+            how_to_play()
+
+        elif choice == '3':
+            start = True
+
+        else:
+            print('Please select either 1, 2 or 3 to continue!')
 
 
-def hangman_instructions():
+def how_to_play():
     """
     Function to display the instructions of the game.
     Allows user to go back to main menu by pressing enter.
     """
+    print('\n')
+    print('Your task in this game is to guess the hidden word behind the\n'
+          'blank spaces. In this hangman game the theme is European\n'
+          'countries. A correct guess will reveal a letter in the name.\n'
+          'A wrong guess will take away a life. Good luck & have fun!')
+
+    input('Press enter to go back to the main menu.')
+    print('\n')
+    main()
+
+
+def main():
+    """
+    Start the application and run the main function.
+    """
+    welcome_message()
+
+
+main()
 
